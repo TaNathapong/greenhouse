26667a2aade5d3e386f39c6ca79c5bb928e7ef0b
minemint <mintviriya@gmail.com>
2017-07-14 09:25:08 +0700

    ยังไง

--- application/modules/member/views/confirm-page.php
+++ application/modules/member/views/confirm-page.php
@@ -1,9 +1,9 @@
 
 
 <div class="container">
-    <form class="form-horizontal" role="form">
+<br/>
         <h2>กรุณาเลือกวิธียืนยัน</h2>
-        <div class="row col-sm-1"></div>
+        <div class="row col-sm-4"></div>
         <div class="col-sm-4">
             <div class="thumbnail">
                 <center>
@@ -12,26 +12,18 @@
             <div class="caption">
                 <center>
                     <div class="product-option-shop">
-                        <a class="add_to_cart_button" data-quantity="1" data-product_sku="" data-product_id="70" rel="nofollow" href="regist-confirm-email.html">ยืนยันผ่านเมลล์</a>
-                    </div>
+                        <a class="add_to_cart_button" data-quantity="1" data-product_sku="" data-product_id="70" rel="nofollow" href="regist-confirm-email.html">ยืนยันผ่านเมล</a>
+                        <h2><a class="shipping-calculator-button" data-toggle="collapse" href="#calcalute-shipping-wrap" aria-expanded="false" aria-controls="calcalute-shipping-wrap">คำนวนค่าขนส่ง</a></h2>
+
+                        <section id="calcalute-shipping-wrap" class="shipping-calculator-form collapse">
+                        <label for="postcode" class="col-sm-3 control-label">รหัสไปรณีย์</label>
+                        
+                            <input type="text" id="postcode" placeholder="" class="form-control" name="mem_shipper_postcode_bill">
+                        
                 </center>
             </div> 
             </div>
         </div>
-        <div class="col-sm-1"></div>
-        <div class="row col-sm-4">
-            <div class="thumbnail">
-                <center>
-                    <img src="images/phone.png" alt="" style="width:150px; height:150px;">
-                </center>
-            <div class="caption">
-                <center>
-                    <div class="product-option-shop">
-                        <a class="add_to_cart_button" data-quantity="1" data-product_sku="" data-product_id="70" rel="nofollow" href="regist-confirm-phone.html">ยืนยันผ่านโทรศัพธ์</a>
-                    </div>
-                </center>
-            </div> 
-            </div> 
-        </div>
-    </form> <!-- /form -->
+        <div class="col-sm-4"></div>
+        
 </div>    
